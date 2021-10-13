80df6659da10bce090780883eb5a63f3ef9308e0
wilkinsc9 <89933816+wilkinsc9@users.noreply.github.com>
2021-10-13 11:52:10 -0400

    Update assignment03.js

--- assignments/assignment03.js
+++ assignments/assignment03.js
@@ -99,6 +99,43 @@ elem[17].children[4].children[1].onclick
 elem[18].children[4].children[1].onclick 
   = function(){sub1(elem[18]);};
 
+// assign a function to the C button
+elem[1].children[4].children[2].onclick 
+  = function(){add1(elem[1]);};
+elem[2].children[4].children[2].onclick 
+  = function(){add1(elem[2]);};
+elem[3].children[4].children[2].onclick 
+  = function(){add1(elem[3]);};
+elem[4].children[4].children[2].onclick 
+  = function(){add1(elem[4]);};
+elem[5].children[4].children[2].onclick 
+  = function(){add1(elem[5]);};
+elem[6].children[4].children[2].onclick 
+  = function(){add1(elem[6]);};
+elem[7].children[4].children[2].onclick 
+  = function(){add1(elem[7]);};
+elem[8].children[4].children[2].onclick 
+  = function(){add1(elem[8]);};
+elem[9].children[4].children[2].onclick 
+  = function(){add1(elem[9]);};
+elem[10].children[4].children[2].onclick 
+  = function(){add1(elem[10]);};
+elem[11].children[4].children[2].onclick 
+  = function(){add1(elem[11]);};
+elem[12].children[4].children[2].onclick 
+  = function(){add1(elem[12]);};
+elem[13].children[4].children[2].onclick 
+  = function(){add1(elem[13]);};
+elem[14].children[4].children[2].onclick 
+  = function(){add1(elem[14]);};
+elem[15].children[4].children[2].onclick 
+  = function(){add1(elem[15]);};
+elem[16].children[4].children[2].onclick 
+  = function(){add1(elem[16]);};
+elem[17].children[4].children[2].onclick 
+  = function(){add1(elem[17]);};
+elem[18].children[4].children[2].onclick 
+  = function(){add1(elem[18]);};
 // create an "add1" function
 function add1 (elem) {
   if(elem.children[2].innerHTML == "-") 
@@ -109,13 +146,13 @@ function add1 (elem) {
     elem.children[2].innerHTML = currentScore + 1;
   }
   if(elem.children[2].innerHTML == "-")
-    elem.children[3].innerHTML == "-";
+    elem.children[3].innerHTML = "-";
   else {
     let currentScore = elem.children[2].innerHTML;
     let par = elem.children[1].innerHTML;
     currentScore = Number.parseInt(currentScore);
     par = Number.parseInt(par);
-    elem.children[3].innerHTML == currentScore - par;
+    elem.children[3].innerHTML = currentScore - par;
   }
 }
 
@@ -129,12 +166,17 @@ function sub1 (elem) {
     elem.children[2].innerHTML = currentScore - 1;
   }
   if(elem.children[2].innerHTML == "-")
-    elem.children[3].innerHTML == "-";
+    elem.children[3].innerHTML = "-";
   else {
     let currentScore = elem.children[2].innerHTML;
     let par = elem.children[1].innerHTML;
     currentScore = Number.parseInt(currentScore);
     par = Number.parseInt(par);
-    elem.children[3].innerHTML == currentScore - par;
+    elem.children[3].innerHTML = currentScore - par;
   }
+}
+
+function c1 (elem) {
+  elem.children[2].innerHTML = "-";
+  elem.children[3].innerHTML = "-";
 }
