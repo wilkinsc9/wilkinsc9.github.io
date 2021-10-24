1c3894b95767f4e8676e156b94e64f041faa2632
wilkinsc9 <89933816+wilkinsc9@users.noreply.github.com>
2021-10-24 15:50:32 -0400

    Update assignment03.js

--- assignments/assignment03.js
+++ assignments/assignment03.js
@@ -155,12 +155,16 @@ function findTotal(){
 function add1 (elem) {
   if(elem.children[2].innerHTML == "-") 
     elem.children[2].innerHTML = "1";
-    document.getElementById("totals").children[2].innerHTML = Number.parseInt(document.getElementById("totals").children[2].innerHTML) + 1;
+    let currentTotal = Number.parseInt(document.getElementById("totals").children[2].innerHTML);
+    currentTotal++;
+    document.getElementById("totals").children[2].innerHTML = currentTotal;
   else {
     let currentScore = elem.children[2].innerHTML;
     currentScore = Number.parseInt(currentScore);
     elem.children[2].innerHTML = currentScore + 1;
-    document.getElementById("totals").children[2].innerHTML = Number.parseInt(document.getElementById("totals").children[2].innerHTML) + 1;
+    let currentTotal = Number.parseInt(document.getElementById("totals").children[2].innerHTML);
+    currentTotal++;
+    document.getElementById("totals").children[2].innerHTML = currentTotal;
   }
   if(elem.children[2].innerHTML == "-")
     elem.children[3].innerHTML = "-";
@@ -170,7 +174,9 @@ function add1 (elem) {
     currentScore = Number.parseInt(currentScore);
     par = Number.parseInt(par);
     elem.children[3].innerHTML = currentScore - par;
-    document.getElementById("totals").children[3].innerHTML = Number.parseInt(document.getElementById("totals").children[3].innerHTML) + currentScore - par;
+    let currentOver = Number.parseInt(document.getElementById("totals").children[3].innerHTML);
+    currentOver = currentOver + (currentScore - par);
+    document.getElementById("totals").children[2].innerHTML = currentOver;
   }
 }
 
@@ -185,7 +191,9 @@ function sub1 (elem) {
     let currentScore = elem.children[2].innerHTML;
     currentScore = Number.parseInt(currentScore);
     elem.children[2].innerHTML = currentScore - 1;
-    document.getElementById("totals").children[2].innerHTML = Number.parseInt(document.getElementById("totals").children[2].innerHTML) - 1;
+    let currentTotal = Number.parseInt(document.getElementById("totals").children[2].innerHTML);
+    currentTotal--;
+    document.getElementById("totals").children[2].innerHTML = currentTotal;
   }
   
   if(elem.children[2].innerHTML == "-")
@@ -196,7 +204,9 @@ function sub1 (elem) {
     currentScore = Number.parseInt(currentScore);
     par = Number.parseInt(par);
     elem.children[3].innerHTML = currentScore - par;
-    document.getElementById("totals").children[3].innerHTML = Number.parseInt(document.getElementById("totals").children[3].innerHTML) + currentScore - par;
+    let currentOver = Number.parseInt(document.getElementById("totals").children[3].innerHTML);
+    currentOver = currentOver + (currentScore - par);
+    document.getElementById("totals").children[2].innerHTML = currentOver;
   }
 }
 
