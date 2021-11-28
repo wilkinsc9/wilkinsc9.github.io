4006fbfbda387be4c12978fd5e6d79ce609dc56c
wilkinsc9 <wilkinsc999@gmail.com>
2021-11-28 17:06:10 -0500

    assignment6

--- assignments/assignment06.js
+++ assignments/assignment06.js
@@ -14,17 +14,21 @@ var loans = [
     
     // pre-fill defaults for first loan year
     var defaultYear = loans[0].loan_year;
-    document.getElementById("loan_year0" + 1).value = defaultYear++;
+    $("[id='loan_year0'+1]").innerHTML(defaultYear++);
+    // document.getElementById("loan_year0" + 1).value = defaultYear++;
     var defaultLoanAmount = loans[0].loan_amount;
-    document.getElementById("loan_amt0" + 1).value = defaultLoanAmount.toFixed(2);
+    $("[id='loan_amt0'+1]").innerHTML(defaultLoanAmount.toFixed(2));
+    // document.getElementById("loan_amt0" + 1).value = defaultLoanAmount.toFixed(2);
     var defaultInterestRate = loans[0].loan_int_rate;
-    document.getElementById("loan_int0" + 1).value = defaultInterestRate;
+    $("[id='loan_int0'+1]").innerHTML(defaultInterestRate);
+    // document.getElementById("loan_int0" + 1).value = defaultInterestRate;
     var loanWithInterest = loans[0].loan_amount * (1 + loans[0].loan_int_rate);
-    document.getElementById("loan_bal0" + 1).innerHTML = toComma(loanWithInterest.toFixed(2));
+    $("[id='loan_bal0'+1]").innerHTML(toComma(loanWithInterest.toFixed(2)));
+    // document.getElementById("loan_bal0" + 1).innerHTML = toComma(loanWithInterest.toFixed(2));
     
     // pre-fill defaults for other loan years
     for(var i=2; i<6; i++) {
-      document.getElementById("loan_year0" + i).value = defaultYear++;
+      $("[id='loan_year0 + i']").innerHTML(defaultYear++);
       document.getElementById("loan_year0" + i).disabled = true;
       document.getElementById("loan_year0" + i).style.backgroundColor = "gray";
       document.getElementById("loan_year0" + i).style.color = "white";
